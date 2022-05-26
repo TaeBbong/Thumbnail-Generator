52980a5fb0b539f2568c2abb8753bf39f2e1cc38
TaeBbong <mok05289@korea.ac.kr>
2022-05-26 23:57:31 +0900

    init: app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# /Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+
+class App:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Hello World")
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
