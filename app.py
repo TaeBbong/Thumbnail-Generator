1cd8e33f7edf564d17ff9ca902854978ea198ec4
TaeBbong <mok05289@korea.ac.kr>
2022-05-28 21:19:18 +0900

    add: color config yaml

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 # /Library/Frameworks/Python.framework/Versions/3.10/bin/python3
 
 from PIL import Image, ImageDraw, ImageFont
+import datetime, json, yaml, random
 
 class App:
     def __init__(self):
-        pass
+        with open('config.yml', 'r') as ymlfile:
+            cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+            self.colors = cfg['colors']
 
-    def create_image(self, filename):
-        img = Image.new('RGB', (200, 200), color = 'white')
+    def input_params(self):
+        self.subject = input("Enter subject name: ")
+        self.title = input("Title: ")
+        self.subtitle = input("Subtitle: ")
+        self.author = input("Author: ")
+        self.date = input("Date: ")
+
+    def create_image(self):
+        img = Image.new('RGB', (1920, 1028), color = 'white')
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 40)
-        draw.text((100, 100), "Hello World", fill=(255, 0, 0), font=font)
-        img.save(filename)
+        draw.text((480, 240), "Hello World", fill=(255, 0, 0), font=font, align="center")
+        img.save(f'{self.date}_{self.subject}.png')
 
 if __name__ == "__main__":
     app = App()
-    app.create_image('test.png')
+    # app.input_params()
+    # app.create_image()
