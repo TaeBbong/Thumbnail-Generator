9d8b27be8e97072f89d1cff494cf70bfc9efb988
TaeBbong <mok05289@korea.ac.kr>
2022-05-28 20:57:03 +0900

    add: base code

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # /Library/Frameworks/Python.framework/Versions/3.10/bin/python3
 
+from PIL import Image, ImageDraw, ImageFont
+
 class App:
     def __init__(self):
         pass
 
-    def run(self):
-        print("Hello World")
+    def create_image(self, filename):
+        img = Image.new('RGB', (200, 200), color = 'white')
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 40)
+        draw.text((100, 100), "Hello World", fill=(255, 0, 0), font=font)
+        img.save(filename)
 
 if __name__ == "__main__":
     app = App()
-    app.run()
+    app.create_image('test.png')
